4ee6bd35a3bec693db8169eecd59ab9fd95b01f4
Michael Parris <super.stemmer@gmail.com>
2013-07-27 23:57:27 -0700

    Initial Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stemmer::Application.config.secret_token = 'd2c394dcf0615667a87ab30f3944ae686a7fdf857bd47f83337fa1831684060cf86078cb1510e0185d2ddef30c0a28abb64707f2d99811d082a7181593c7feaf'
